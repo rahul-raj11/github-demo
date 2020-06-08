848107867ad6d7cc9eff624a6db0296c67fb67e6
Rahul Raj <rahul-raj@hcl.com>
2020-06-09 01:58:23 +0530

    Second commit for SampleClass file

--- SampleClass.java
+++ SampleClass.java
@@ -1,8 +1,12 @@
 class SampleClass {
 	public static void main(String[] args) {
 		
-		public static void m1() {
-			Ststem.out.println("First method");
+		public static void m2() {
+			Ststem.out.println("Second method");
+		}
+		
+		public static void m3() {
+			Ststem.out.println("Third method");
 		}
 	}
 }	
