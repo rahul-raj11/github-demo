acb71723c11c334304b3b227bfef88b55f818cb1
Rahul Raj <rahul-raj@hcl.com>
2020-06-09 17:40:24 +0530

    Class with 1 method

--- SampleClass.java
+++ SampleClass.java
@@ -1,18 +1,7 @@
 class SampleClass {
 	public static void main(String[] args) {
 		
-		public static void m2() {
-			Ststem.out.println("Second method");
-		}
-		
-		public static void m3() {
-			Ststem.out.println("Third method");
-		}
-		
 		public static void m1() {
-			Ststem.out.println("Third method");
-		}
-		public static void m4() {
 			Ststem.out.println("Third method");
 		}
 	}
